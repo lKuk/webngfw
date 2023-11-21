08db9da83e12a45d31720f4ee672e35f482c35d6
lkaa <boxkaa@yandex.ru>
2023-11-21 17:26:35 +0300

    Сделал CodeReView скриптов

--- ngfwadmin/forms.py
+++ ngfwadmin/forms.py
@@ -4,32 +4,32 @@ from django.core.validators import *
 
 
 # Устройства NGFW
-class ConnectForm(forms.Form):
-        ip = forms.CharField(max_length=255,
-                             label='IP адрес',
-                             initial='192.168.1.145',
-                             widget=forms.TextInput(attrs={'class': 'form-control'}))
-        port = forms.CharField(max_length=255,
-                               label='Порт',
-                               initial='18888',
-                               widget=forms.TextInput(attrs={'class': 'form-control'}))
-        login = forms.CharField(max_length=255,
-                                label='Логин',
-                                initial='admin',
-                                widget=forms.TextInput(attrs={'class': 'form-control'}))
-        password = forms.CharField(max_length=255,
-                                   label='Пароль',
-                                   initial='111111',
-                                   widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'password'}))
-    #
-    # class Meta:
-    #     model = Device
-    #     fields = "__all__"
-    #
-    # def clean_ip(self):
-    #     ip = self.cleaned_data['ip']
-    #     validate_ipv46_address(ip)
-    #
-    # def clean_port(self):
-    #     port = self.cleaned_data['port']
-    #     MaxValueValidator(port, 65535)
+class ConnectForm(forms.ModelForm):
+    ip = forms.CharField(max_length=255,
+                         label='IP адрес',
+                         initial='192.168.1.145',
+                         widget=forms.TextInput(attrs={'class': 'form-control'}))
+    port = forms.CharField(max_length=255,
+                           label='Порт',
+                           initial='18888',
+                           widget=forms.TextInput(attrs={'class': 'form-control'}))
+    login = forms.CharField(max_length=255,
+                            label='Логин',
+                            initial='admin',
+                            widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(max_length=255,
+                               label='Пароль',
+                               initial='111111',
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'password'}))
+
+    class Meta:
+        model = Device
+        fields = ["ip", "port", "login", "password"]
+
+    def clean_ip(self):
+        ip = self.cleaned_data['ip']
+        validate_ipv46_address(ip)
+
+    def clean_port(self):
+        port = self.cleaned_data['port']
+        MaxValueValidator(port, 65535)
