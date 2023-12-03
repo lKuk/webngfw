40fa786faeb2fac15e015a39816157fda0233a09
lkaa <boxkaa@yandex.ru>
2023-12-03 14:47:35 +0300

    Ajax step 2

--- ngfwadmin/views/sys/sys.py
+++ ngfwadmin/views/sys/sys.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+
 from ngfwadmin.rest.sys.sys import *
 from ngfwadmin.views.connect.connect import *
 from ngfwadmin.rest.monitoring.monitoring import *
@@ -34,21 +36,18 @@ def sys_ajax():
     # Подключение
     dev = get_connect()
     if 'url' not in dev:
-        return context
-    try:
-        # Получить данные
-        url = dev.get('url')
-        uptime = uptime_get(url)
-        status = status_get(url)
-        ram = monitoring_ram_get(url)
-        disk = monitoring_disk_get(url)
-        lcores = monitoring_lcores_get(url)
-        # Наполнить содержимое
-        context['ram'] = ram
-        context['disk'] = disk
-        context['lcores'] = lcores
-        context['uptime'] = uptime
-        context['status'] = status
-    except:
-        return context
-    return context
+        return JsonResponse(context)
+    # Получить данные
+    url = dev.get('url')
+    uptime = uptime_get(url)
+    status = status_get(url)
+    ram = monitoring_ram_get(url)
+    disk = monitoring_disk_get(url)
+    lcores = monitoring_lcores_get(url)
+    # Наполнить содержимое
+    context['ram'] = ram
+    context['disk'] = disk
+    context['lcores'] = lcores
+    context['uptime'] = uptime
+    context['status'] = status
+    return JsonResponse(context)
