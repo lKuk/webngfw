ca73a2e4817a423e0cf3fdb5a0304bf075d49523
lKuk <boxkaa@yandex.ru>
2023-12-13 13:55:06 +0300

    Подключение работает в зависимости от ключа сессии

--- ngfwadmin/views/debug/error.py
+++ ngfwadmin/views/debug/error.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 
+from ngfwadmin.views.connect.dev import dev_get
+
 
 # Страница ошибки
 def error(request):
     # Подключение
-    context = {'name': 'Error: 404 страница не найдена'}
+    dev = dev_get(request)
+    # Содержимое ошибки
+    context = {'dev': dev,
+               'name': 'Error: 404 страница не найдена'}
     return render(request, 'debug/error.html', context=context)
 
 
 # Страница исключений
 def exception(request, ex):
+    # Подключение
+    dev = dev_get(request)
+    # Содержимое ошибки
     context = {'ex': ex,
+               'dev': dev,
                'name': 'Exception'}
     return render(request, 'debug/error.html', context=context)
 
